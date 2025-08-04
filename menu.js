974ff466f55160009312492855db026c299e3419
John Coonrod <jcoonrod51@gmail.com>
2025-08-04 09:47:06 -0400

    add files

--- menu.js
+++ menu.js
@@ -3,7 +3,13 @@ const navbar=`
 <nav>
   <ul>
     <li><a href=/>Bienvenu !</a></li>
-    <li><a href=/qui.html>Qui sommes-nous ?</a></li>
+    <li><a href=/qui.html>Qui sommes-nous ?</a>
+    <ul>
+    <li><a href=/equipe>Équipe de direction</a></li>
+    <li><a href=/conseil>Conseil administratif</a></li>
+    <li><a href=/onu.html>Ave l'ONU</a></li>
+    </ul>
+    </li>
     <li><a href=/que.html>Que faisons-nous ?</a></li>
   </ul>
 </nav>
